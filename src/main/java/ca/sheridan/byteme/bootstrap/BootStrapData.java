c1838709a7334e03d8aa18e93e61a949877426ba
Charlie Nguyen <ng.charlie123@gmail.com>
2025-11-25 16:04:58 -0500

    feat: Update bootstrap data with more diverse orders

--- src/main/java/ca/sheridan/byteme/bootstrap/BootStrapData.java
+++ src/main/java/ca/sheridan/byteme/bootstrap/BootStrapData.java
@@ -103,9 +103,11 @@ public class BootStrapData implements CommandLineRunner {
         // --- seed a couple of orders (only if none exist for this customer) ---
         if (orderRepository.findByUserId(customer.getId()).isEmpty()) {
             CartItem item1 = new CartItem("prod_12345", "Large Cookiegram", "Happy Birthday!", "Royal Blue", "#4169E1", 29.99);
-            CartItem item2 = new CartItem("prod_67890", "Large Cookiegram", "", "", "", 24.99);
+            CartItem item2 = new CartItem("prod_67890", "Large Cookiegram", "Get Well Soon!", "Green", "#008000", 24.99);
+            CartItem item3 = new CartItem("prod_ABCDE", "Small Cookiegram", "Congratulations!", "Red", "#FF0000", 19.99);
 
             Order order1 = Order.builder()
+                .id("ORD001")
                 .userId(customer.getId())
                 .items(List.of(item1))
                 .subtotal(29.99)
@@ -120,6 +122,7 @@ public class BootStrapData implements CommandLineRunner {
                 .build();
             
             Order order2 = Order.builder()
+                .id("ORD002")
                 .userId(customer.getId())
                 .items(List.of(item2))
                 .subtotal(24.99)
@@ -129,13 +132,46 @@ public class BootStrapData implements CommandLineRunner {
                 .orderDate(LocalDateTime.now().minusDays(2))
                 .deliveryDate(LocalDate.now().plusDays(1))
                 .status(Status.Shipped)
-                .shippingAddress(ShippingAddress.builder().name("Customer User").addressLine1("123 Main St").city("Toronto").province("ON").postalCode("M1M1M1").country("Canada").build())
-                .billingAddress(BillingAddress.builder().name("Customer User").addressLine1("123 Main St").city("Toronto").province("ON").postalCode("M1M1M1").country("Canada").build())
+                .shippingAddress(ShippingAddress.builder().name("Customer User").addressLine1("456 Oak St").city("Mississauga").province("ON").postalCode("L5B2C9").country("Canada").build())
+                .billingAddress(BillingAddress.builder().name("Customer User").addressLine1("456 Oak St").city("Mississauga").province("ON").postalCode("L5B2C9").country("Canada").build())
                 .carrier("FedEx")
                 .trackingNumber("123456789")
                 .build();
 
-            orderRepository.saveAll(List.of(order1, order2));
+            Order order3 = Order.builder()
+                .id("ORD003")
+                .userId(customer.getId())
+                .items(List.of(item3))
+                .subtotal(19.99)
+                .tax(2.60)
+                .shippingCost(5.00)
+                .total(27.59)
+                .orderDate(LocalDateTime.now().minusDays(1))
+                .deliveryDate(LocalDate.now().plusDays(2))
+                .status(Status.Baking)
+                .shippingAddress(ShippingAddress.builder().name("Customer User").addressLine1("789 Pine St").city("Brampton").province("ON").postalCode("L6T0G8").country("Canada").build())
+                .billingAddress(BillingAddress.builder().name("Customer User").addressLine1("789 Pine St").city("Brampton").province("ON").postalCode("L6T0G8").country("Canada").build())
+                .build();
+
+            // Order for another user to test security
+            User anotherCustomer = User.builder().name("Another Customer").email("another@cookie.com").password(passwordEncoder.encode("Password123!")).role(Role.CUSTOMER).build();
+            userRepository.save(anotherCustomer);
+            Order anotherOrder = Order.builder()
+                .id("ORD004")
+                .userId(anotherCustomer.getId())
+                .items(List.of(new CartItem("prod_XYZ", "Large Cookiegram", "Secret Message", "Black", "#000000", 29.99)))
+                .subtotal(29.99)
+                .tax(3.90)
+                .shippingCost(5.00)
+                .total(38.89)
+                .orderDate(LocalDateTime.now().minusDays(5))
+                .deliveryDate(LocalDate.now().minusDays(2))
+                .status(Status.Delivered)
+                .shippingAddress(ShippingAddress.builder().name("Another Customer").addressLine1("1 Hacker Way").city("Menlo Park").province("CA").postalCode("94025").country("USA").build())
+                .billingAddress(BillingAddress.builder().name("Another Customer").addressLine1("1 Hacker Way").city("Menlo Park").province("CA").postalCode("94025").country("USA").build())
+                .build();
+
+            orderRepository.saveAll(List.of(order1, order2, order3, anotherOrder));
             System.out.println("Seeded demo orders for customer.");
         }
     }
